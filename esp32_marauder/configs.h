11872c2dda34daf9416837fc57136c565b4a9954
Just Call Me Koko <at.the.red.team@gmail.com>
2022-05-28 08:43:15 -0400

    Create bins for release v0.9.6

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -6,6 +6,8 @@
   
   //#define MARAUDER_MINI
   #define MARAUDER_V4
+  //#define MARAUDER_V6
+  //#define MARAUDER_KIT
   //#define GENERIC_ESP32
   //#define MARAUDER_FLIPPER
 
@@ -77,7 +79,116 @@
     #define STATUSBAR_COLOR 0x4A49
     
     #define KIT_LED_BUILTIN 13
+  #endif
+
+  #ifdef MARAUDER_V6
+    #define BANNER_TEXT_SIZE 2
+
+    #ifndef TFT_WIDTH
+      #define TFT_WIDTH 240
+    #endif
+
+    #ifndef TFT_HEIGHT
+      #define TFT_HEIGHT 320
+    #endif
+
+    #define TFT_DIY
+    
+    #define SCREEN_WIDTH TFT_WIDTH
+    #define SCREEN_HEIGHT TFT_HEIGHT
+    #define HEIGHT_1 TFT_WIDTH
+    #define WIDTH_1 TFT_HEIGHT
+    #define STANDARD_FONT_CHAR_LIMIT (TFT_WIDTH/6) // number of characters on a single line with normal font
+    #define TEXT_HEIGHT 16 // Height of text to be printed and scrolled
+    #define BOT_FIXED_AREA 0 // Number of lines in bottom fixed area (lines counted from bottom of screen)
+    #define TOP_FIXED_AREA 48 // Number of lines in top fixed area (lines counted from top of screen)
+    #define YMAX 320 // Bottom of screen area
+    #define minimum(a,b)     (((a) < (b)) ? (a) : (b))
+    //#define MENU_FONT NULL
+    #define MENU_FONT &FreeMono9pt7b // Winner
+    //#define MENU_FONT &FreeMonoBold9pt7b
+    //#define MENU_FONT &FreeSans9pt7b
+    //#define MENU_FONT &FreeSansBold9pt7b
+    #define BUTTON_ARRAY_LEN 9
+    #define STATUS_BAR_WIDTH 16
+    #define LVGL_TICK_PERIOD 6
+    
+    #define FRAME_X 100
+    #define FRAME_Y 64
+    #define FRAME_W 120
+    #define FRAME_H 50
+    
+    // Red zone size
+    #define REDBUTTON_X FRAME_X
+    #define REDBUTTON_Y FRAME_Y
+    #define REDBUTTON_W (FRAME_W/2)
+    #define REDBUTTON_H FRAME_H
+    
+    // Green zone size
+    #define GREENBUTTON_X (REDBUTTON_X + REDBUTTON_W)
+    #define GREENBUTTON_Y FRAME_Y
+    #define GREENBUTTON_W (FRAME_W/2)
+    #define GREENBUTTON_H FRAME_H
+    
+    #define STATUSBAR_COLOR 0x4A49
+    
+    #define KIT_LED_BUILTIN 13
   #endif 
+
+  #ifdef MARAUDER_KIT
+    #define BANNER_TEXT_SIZE 2
+
+    #ifndef TFT_WIDTH
+      #define TFT_WIDTH 240
+    #endif
+
+    #ifndef TFT_HEIGHT
+      #define TFT_HEIGHT 320
+    #endif
+
+    #define TFT_DIY
+    #define KIT
+    
+    #define SCREEN_WIDTH TFT_WIDTH
+    #define SCREEN_HEIGHT TFT_HEIGHT
+    #define HEIGHT_1 TFT_WIDTH
+    #define WIDTH_1 TFT_HEIGHT
+    #define STANDARD_FONT_CHAR_LIMIT (TFT_WIDTH/6) // number of characters on a single line with normal font
+    #define TEXT_HEIGHT 16 // Height of text to be printed and scrolled
+    #define BOT_FIXED_AREA 0 // Number of lines in bottom fixed area (lines counted from bottom of screen)
+    #define TOP_FIXED_AREA 48 // Number of lines in top fixed area (lines counted from top of screen)
+    #define YMAX 320 // Bottom of screen area
+    #define minimum(a,b)     (((a) < (b)) ? (a) : (b))
+    //#define MENU_FONT NULL
+    #define MENU_FONT &FreeMono9pt7b // Winner
+    //#define MENU_FONT &FreeMonoBold9pt7b
+    //#define MENU_FONT &FreeSans9pt7b
+    //#define MENU_FONT &FreeSansBold9pt7b
+    #define BUTTON_ARRAY_LEN 9
+    #define STATUS_BAR_WIDTH 16
+    #define LVGL_TICK_PERIOD 6
+    
+    #define FRAME_X 100
+    #define FRAME_Y 64
+    #define FRAME_W 120
+    #define FRAME_H 50
+    
+    // Red zone size
+    #define REDBUTTON_X FRAME_X
+    #define REDBUTTON_Y FRAME_Y
+    #define REDBUTTON_W (FRAME_W/2)
+    #define REDBUTTON_H FRAME_H
+    
+    // Green zone size
+    #define GREENBUTTON_X (REDBUTTON_X + REDBUTTON_W)
+    #define GREENBUTTON_Y FRAME_Y
+    #define GREENBUTTON_W (FRAME_W/2)
+    #define GREENBUTTON_H FRAME_H
+    
+    #define STATUSBAR_COLOR 0x4A49
+    
+    #define KIT_LED_BUILTIN 13
+  #endif
   
   #ifdef MARAUDER_MINI
     #define TFT_MISO 19
@@ -159,6 +270,44 @@
     #define BUTTON_PADDING 22
     //#define BUTTON_ARRAY_LEN 5
   #endif
+
+  #ifdef MARAUDER_V6
+    #define BANNER_TIME 100
+    
+    #define COMMAND_PREFIX "!"
+    
+    // Keypad start position, key sizes and spacing
+    #define KEY_X 120 // Centre of key
+    #define KEY_Y 50
+    #define KEY_W 240 // Width and height
+    #define KEY_H 22
+    #define KEY_SPACING_X 0 // X and Y gap
+    #define KEY_SPACING_Y 1
+    #define KEY_TEXTSIZE 1   // Font size multiplier
+    #define ICON_W 22
+    #define ICON_H 22
+    #define BUTTON_PADDING 22
+    //#define BUTTON_ARRAY_LEN 5
+  #endif
+
+  #ifdef MARAUDER_KIT
+    #define BANNER_TIME 100
+    
+    #define COMMAND_PREFIX "!"
+    
+    // Keypad start position, key sizes and spacing
+    #define KEY_X 120 // Centre of key
+    #define KEY_Y 50
+    #define KEY_W 240 // Width and height
+    #define KEY_H 22
+    #define KEY_SPACING_X 0 // X and Y gap
+    #define KEY_SPACING_Y 1
+    #define KEY_TEXTSIZE 1   // Font size multiplier
+    #define ICON_W 22
+    #define ICON_H 22
+    #define BUTTON_PADDING 22
+    //#define BUTTON_ARRAY_LEN 5
+  #endif
   
   #ifdef MARAUDER_MINI
     #define BANNER_TIME 50
@@ -184,6 +333,14 @@
     #define SD_CS 12
   #endif
 
+  #ifdef MARAUDER_V6
+    #define SD_CS 12
+  #endif
+
+  #ifdef MARAUDER_KIT
+    #define SD_CS 12
+  #endif
+
   #ifdef MARAUDER_MINI
     #define SD_CS 4
   #endif
@@ -200,6 +357,16 @@
   #endif
 
   #ifdef MARAUDER_V4
+    #define HAS_SCREEN
+    #define HAS_BT
+  #endif
+
+  #ifdef MARAUDER_V6
+    #define HAS_SCREEN
+    #define HAS_BT
+  #endif
+
+  #ifdef MARAUDER_KIT
     #define HAS_SCREEN
     #define HAS_BT
   #endif
