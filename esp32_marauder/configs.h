cbe6d0196b4947e3bf97b6ca784d4bcb26ae8d48
Just Call Me Koko <at.the.red.team@gmail.com>
2023-05-04 14:59:46 -0400

    Revert config

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -9,11 +9,11 @@
   //#define WRITE_PACKETS_SERIAL
   
   //#define MARAUDER_MINI
-  #define MARAUDER_V4
+  //#define MARAUDER_V4
   //#define MARAUDER_V6
   //#define MARAUDER_KIT
   //#define GENERIC_ESP32
-  //#define MARAUDER_FLIPPER
+  #define MARAUDER_FLIPPER
   //#define ESP32_LDDB
   //#define MARAUDER_DEV_BOARD_PRO
 
