76f8ceb63f8e89abc1dc07dbe1ce3aeaa144aa20
Just Call Me Koko <at.the.red.team@gmail.com>
2023-07-14 11:24:07 -0400

    Upgrade arduino-ep32 to 2.0.9

--- esp32_marauder/EvilPortal.cpp
+++ esp32_marauder/EvilPortal.cpp
@@ -1,5 +1,26 @@
 #include "EvilPortal.h"
 
 EvilPortal::EvilPortal() {
+  this->runServer = false;
+  this->name_received = false;
+  this->password_received = false;
+}
+
+String EvilPortal::get_user_name() {
+  return this->user_name;
+}
+
+String EvilPortal::get_password() {
+  return this->password;
+}
+
+void EvilPortal::startAP() {
   
+}
+
+void EvilPortal::startPortal() {
+  // wait for flipper input to get config index
+  this->startAP();
+
+  this->runServer = true;
 }
--- esp32_marauder/EvilPortal.h
+++ esp32_marauder/EvilPortal.h
@@ -1,17 +1,48 @@
 #ifndef EvilPortal_h
 #define EvilPortal_h
 
+#include "ESPAsyncWebServer.h"
+#include <AsyncTCP.h>
+#include <DNSServer.h>
+
 #include "configs.h"
 #include "settings.h"
+#include "WiFiScan.h"
 
 extern Settings settings_obj;
+extern WiFiScan wifi_scan_obj;
+
+#define WAITING 0
+#define GOOD 1
+#define BAD 2
+
+#define SET_HTML_CMD "sethtml="
+#define SET_AP_CMD "setap="
+#define RESET_CMD "reset"
+#define START_CMD "start"
+#define ACK_CMD "ack"
+#define MAX_HTML_SIZE 20000
 
 class EvilPortal {
 
   private:
+    bool runServer;
+    bool name_received;
+    bool password_received;
+    String user_name;
+    String password;
+
+    //DNSServer dnsServer;
+    //AsyncWebServer server(80);
+
+    void startPortal();
+    void startAP();
 
   public:
     EvilPortal();
+
+    String get_user_name();
+    String get_password();
 
 };
 
