5a825c3a0a458d7171681312bfdef496ed5bad65
Pirata <104320209+bmorcelli@users.noreply.github.com>
2024-02-28 13:04:37 -0300

    Update stickcLED.cpp

--- esp32_marauder/stickcLED.cpp
+++ esp32_marauder/stickcLED.cpp
@@ -1,53 +1,63 @@
 #include "stickcLED.h"
 // NB M5Stick C Plus LED is active low, so digitalWrite() calls are inverted
+
+#ifdef MARAUDER_M5STICKCP2
+    #define M5LED_ON HIGH
+    #define M5LED_OFF LOW
+#else
+    #define M5LED_ON LOW
+    #define M5LED_OFF HIGH
+#endif
+
 void stickcLED::RunSetup() {
     pinMode(STICKC_LED_PIN, OUTPUT);
 
 if (!settings_obj.loadSetting<bool>("EnableLED")) {
-    digitalWrite(STICKC_LED_PIN, HIGH);
+    digitalWrite(STICKC_LED_PIN, M5LED_OFF);
     return;
 }
 
 delay(50);
 
-  digitalWrite(STICKC_LED_PIN, LOW);
+  digitalWrite(STICKC_LED_PIN, M5LED_ON);
   delay(500);
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
   delay(250);
-  digitalWrite(STICKC_LED_PIN, LOW);
+  digitalWrite(STICKC_LED_PIN, M5LED_ON);
   delay(500);
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
   delay(250);
-  digitalWrite(STICKC_LED_PIN, LOW);
+  digitalWrite(STICKC_LED_PIN, M5LED_ON);
   delay(500);
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
 }
 
 void stickcLED::attackLED() {
   if (!settings_obj.loadSetting<bool>("EnableLED"))
     return;
     
-  digitalWrite(STICKC_LED_PIN, LOW);
+  digitalWrite(STICKC_LED_PIN, M5LED_ON);
   delay(300);
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
 }
 
 void stickcLED::sniffLED() {
   if (!settings_obj.loadSetting<bool>("EnableLED"))
     return;
     
-  digitalWrite(STICKC_LED_PIN, LOW);
+  digitalWrite(STICKC_LED_PIN, M5LED_ON);
   delay(300);
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
 }
 
 void stickcLED::offLED() {
   if (!settings_obj.loadSetting<bool>("EnableLED"))
     return;
   
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
 }
 
 void stickcLED::main() {
   // do nothing
 }
+
