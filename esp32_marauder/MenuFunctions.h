22f9b35ec701749062b7251179c333a121145ef9
Angus Ainslie <angus@kaake.ca>
2024-03-31 12:21:13 -0700

    MenuFunctions.h: lilygo T3 display uses a different lv library

--- esp32_marauder/MenuFunctions.h
+++ esp32_marauder/MenuFunctions.h
@@ -76,8 +76,13 @@ extern Settings settings_obj;
 PROGMEM void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p);
 PROGMEM bool my_touchpad_read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data);
 
+#ifdef LILYGO_T_DISPLAY_S3R8
+PROGMEM static lv_disp_drv_t disp_buf;
+PROGMEM static lv_color_t buf[TFT_WIDTH * 10];
+#else
 PROGMEM static lv_disp_buf_t disp_buf;
 PROGMEM static lv_color_t buf[LV_HOR_RES_MAX * 10];
+#endif
 
 PROGMEM static void ta_event_cb(lv_obj_t * ta, lv_event_t event);
 PROGMEM static void add_ssid_keyboard_event_cb(lv_obj_t * keyboard, lv_event_t event);
