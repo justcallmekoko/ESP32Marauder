025e19f1152d270a855fd7f9a3ab009da8740b1c
Pirata <104320209+bmorcelli@users.noreply.github.com>
2025-02-01 13:14:22 +0000

    C Plus2

--- User_Setup_Select.h
+++ User_Setup_Select.h
@@ -26,6 +26,7 @@
 //#include <User_Setup_marauder_mini.h>
 //#include <User_Setup_marauder_rev_feather.h>
 //#include <User_Setup_marauder_m5stickc.h>
+//#include <User_Setup_marauder_m5stickcp2.h>
 //#include <User_Setup_pocket_sdr.h>
 #include <User_Setup_dual_nrf24.h>
 //#include <User_Setup_pocket_sdr_2.h>
--- esp32_marauder/stickcLED.cpp
+++ esp32_marauder/stickcLED.cpp
@@ -1,51 +1,62 @@
 #include "stickcLED.h"
 // NB M5Stick C Plus LED is active low, so digitalWrite() calls are inverted
+
+#ifdef MARAUDER_M5STICKCP2
+    #define M5LED_ON HIGH
+    #define M5LED_OFF LOW
+    #define STICKC_LED_PIN 19
+#else
+    #define M5LED_ON LOW
+    #define M5LED_OFF HIGH
+    #define STICKC_LED_PIN 10
+#endif
+
 void stickcLED::RunSetup() {
     pinMode(STICKC_LED_PIN, OUTPUT);
 
 if (!settings_obj.loadSetting<bool>("EnableLED")) {
-    digitalWrite(STICKC_LED_PIN, HIGH);
+    digitalWrite(STICKC_LED_PIN, M5LED_OFF);
     return;
 }
 
 delay(50);
 
-  digitalWrite(STICKC_LED_PIN, LOW);
+  digitalWrite(STICKC_LED_PIN, M5LED_ON);
   delay(500);
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
   delay(250);
-  digitalWrite(STICKC_LED_PIN, LOW);
+  digitalWrite(STICKC_LED_PIN, M5LED_ON);
   delay(500);
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
   delay(250);
-  digitalWrite(STICKC_LED_PIN, LOW);
+  digitalWrite(STICKC_LED_PIN, M5LED_ON);
   delay(500);
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
 }
 
 void stickcLED::attackLED() {
   if (!settings_obj.loadSetting<bool>("EnableLED"))
     return;
     
-  digitalWrite(STICKC_LED_PIN, LOW);
+  digitalWrite(STICKC_LED_PIN, M5LED_ON);
   delay(300);
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
 }
 
 void stickcLED::sniffLED() {
   if (!settings_obj.loadSetting<bool>("EnableLED"))
     return;
     
-  digitalWrite(STICKC_LED_PIN, LOW);
+  digitalWrite(STICKC_LED_PIN, M5LED_ON);
   delay(300);
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
 }
 
 void stickcLED::offLED() {
   if (!settings_obj.loadSetting<bool>("EnableLED"))
     return;
   
-  digitalWrite(STICKC_LED_PIN, HIGH);
+  digitalWrite(STICKC_LED_PIN, M5LED_OFF);
 }
 
 void stickcLED::main() {
