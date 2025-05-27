7f68fa3aea3cd93bee56590b39e725034ef3c2a7
amec0e <88857687+amec0e@users.noreply.github.com>
2025-05-27 22:57:23 +0100

    Update MenuFunctions.h
    Added Pinescan and MultiSSID

--- esp32_marauder/MenuFunctions.h
+++ esp32_marauder/MenuFunctions.h
@@ -74,6 +74,8 @@ extern Settings settings_obj;
 #define DISABLE_TOUCH 34
 #define FLIPPER 35
 #define BLANK 36
+//#define PINEAPPLE 36
+//#define MULTISSID 36
 
 PROGMEM void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p);
 PROGMEM bool my_touchpad_read(lv_indev_drv_t * indev_driver, lv_indev_data_t * data);
