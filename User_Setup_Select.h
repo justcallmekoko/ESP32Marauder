335e72514499709d2df9da9d5c29a02a01cda394
Just Call Me Koko <at.the.red.team@gmail.com>
2025-06-24 13:11:26 -0400

    Increment version number

--- User_Setup_Select.h
+++ User_Setup_Select.h
@@ -30,6 +30,7 @@
 //#include <User_Setup_pocket_sdr.h>
 //#include <User_Setup_dual_nrf24.h>
 //#include <User_Setup_pocket_sdr_2.h>
+//#include <User_Setup_pocket_sdr_3.h>
 //#include <User_Setup_cyd_micro.h>
 //#include <User_Setup_cyd_guition.h>
 //#include <User_Setup_cyd_2usb.h>
