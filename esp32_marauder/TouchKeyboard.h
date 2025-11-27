84416970e76c592afb8d471c875d756e125995b0
Just Call Me Koko <at.the.red.team@gmail.com>
2025-11-27 03:08:04 -0500

    Remove LVGL completely

--- esp32_marauder/TouchKeyboard.h
+++ esp32_marauder/TouchKeyboard.h
@@ -9,9 +9,12 @@
 #include <stdint.h>
 
 enum KeyboardResult {
-    KB_NONE = 0,
-    KB_CHANGED,
-    KB_DONE
+  KB_NONE = 0,
+  KB_CHANGED,
+  KB_DONE,
+  KB_CANCEL,
+  KB_TOGGLE_LAYOUT,
+  KB_TOGGLE_CAPS
 };
 
 /**
@@ -21,8 +24,7 @@ enum KeyboardResult {
  * @param bufLen   Total size of the buffer (including space for '\0').
  * @param title    Optional title displayed above the text box (can be nullptr).
  *
- * @return true if user pressed OK, false if aborted (currently: you’d have to
- *         add your own external abort condition – e.g. button press).
+ * @return true if user pressed OK, false if user pressed CANCEL (or if buffer invalid).
  */
 bool keyboardInput(char *buffer, size_t bufLen, const char *title = nullptr);
 
