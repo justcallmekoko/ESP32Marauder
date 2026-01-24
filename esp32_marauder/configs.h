cd6d0dda72aa43ef3e848f41a084f4e3035d7488
Just Call Me Koko <at.the.red.team@gmail.com>
2026-01-24 11:13:19 -0500

    Fix macros

--- esp32_marauder/configs.h
+++ esp32_marauder/configs.h
@@ -6,7 +6,7 @@
 
   #define POLISH_POTATO
 
-  #define DEVELOPER
+  //#define DEVELOPER
 
   //// BOARD TARGETS
   //#define MARAUDER_M5STICKC
@@ -14,7 +14,7 @@
   //#define MARAUDER_MINI
   //#define MARAUDER_V4
   //#define MARAUDER_V6
-  #define MARAUDER_V6_1
+  //#define MARAUDER_V6_1
   //#define MARAUDER_V7
   //#define MARAUDER_V7_1
   //#define MARAUDER_KIT
