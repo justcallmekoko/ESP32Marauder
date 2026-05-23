d608137f683e37b6b926383a02fe31c7125b4662
Peter Shipley <peter.shipley@gmail.com>
2026-05-22 20:26:25 -0700

    add menu option to rescanr/probe for SD card

--- esp32_marauder/MenuFunctions.cpp
+++ esp32_marauder/MenuFunctions.cpp
@@ -2797,6 +2797,15 @@ void MenuFunctions::RunSetup()
     wifi_scan_obj.RunLoadATList();
   });
 
+#ifdef HAS_SD
+  if(!sd_obj.supported) {
+      this->addNodes(&saveFileMenu, "Rescan SD", TFTWHITE, NULL, SD_UPDATE, [this]() {
+        this->changeMenu(&loadAPsMenu, true);
+        sd_obj.initSD();
+      });
+  }
+#endif
+
   saveSSIDsMenu.parentMenu = &saveFileMenu;
   this->addNodes(&saveSSIDsMenu, text09, TFTLIGHTGREY, NULL, 0, [this]() {
     this->changeMenu(saveSSIDsMenu.parentMenu, true);
