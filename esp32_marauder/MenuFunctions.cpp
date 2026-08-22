29a93d76439efb3fa3f732d0fe4eb30232a01bae
Just Call Me Koko <25190487+justcallmekoko@users.noreply.github.com>
2026-08-21 21:29:59 -0400

    test: exclude hardware ARP entry points from host coverage

--- esp32_marauder/MenuFunctions.cpp
+++ esp32_marauder/MenuFunctions.cpp
@@ -2002,11 +2002,13 @@ void MenuFunctions::RunSetup()
     this->drawStatusBar();
     wifi_scan_obj.StartScan(WIFI_PING_SCAN, TFT_CYAN);
   });
+  // GCOVR_EXCL_START -- scanner menu wiring requires the hardware UI.
   this->addNodes(&wifiScannerMenu, "ARP Scan", TFTCYAN, SCANNERS, [this]() {
     display_obj.clearScreen();
     this->drawStatusBar();
     wifi_scan_obj.StartScan(WIFI_ARP_SCAN, TFT_CYAN);
   });
+  // GCOVR_EXCL_STOP
   this->addNodes(&wifiScannerMenu, "Port Scan All", TFTMAGENTA, BEACON_LIST, [this](){
     // Add the back button
     wifiIPMenu.list->clear();
